82a6264a0e031ac6d7c44fe454f2de306ac747f5
aelmemy <aelmemy@arabdt.com>
2022-08-05 19:25:44 +0200

    Page Controller

---
+++ new02/ِExtensions/String Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Extension.swift
+//  new02
+//
+//  Created by El-Memy on 03/08/2022.
+//
+
+import Foundation
+
+extension String {
+    var localized : String{
+        return NSLocalizedString(self, comment: "")
+    }
+}
